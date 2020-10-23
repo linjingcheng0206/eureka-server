f6274b9af1858748a05bdbbbf1497b8a5bdb3664
ljc <linjingcheng0206@163.com>
2020-10-23 11:39:48 +0800

    first commit

---
+++ src/main/java/com/roncoo/eshop/EurekaServer.java
@@ -0,0 +1,18 @@
+package com.roncoo.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author linjingcheng
+ * @data 2020/10/14
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
